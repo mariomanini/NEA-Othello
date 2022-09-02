d4ce42a5af18ab0cd1f9cced21e66c537ae7dd7a
TABLET-KSAM6KJT\Mario <mario.manini@icloud.com>
2022-09-02 16:56:44 +0100

    Added flipping the counters in all diagonal directions.

--- Othello.py
+++ Othello.py
@@ -19,12 +19,13 @@ class Game():
     output = output + "\n" + (f"{self.__player} to play: ")
     return output
 
-  
+
   def flip(self,row,col):
     row -= 1
     col -= 1
     #Normally would be 7,5
     #Indexing means it would be 6,4
+    
     for i in range(row-1,0,-1):
       if self.__board[i][col] == self.__player:
         for b in range(i+1,row):
@@ -65,17 +66,92 @@ class Game():
           else:
             break
 
+    trd = 1
+    trdrows = []
+    trdcolumns = []
+    trFound = False
+    try:
+      while trFound == False and row-trd >= 0 and col+trd <= 7:
+        trdrows.append(row-trd)
+        trdcolumns.append(col+trd)
+        if self.__board[row-trd][col+trd] == self.__player:
+          trFound = True
+          for i in range(len(trdrows)):
+            self.__board[trdrows[i]][trdcolumns[i]] = self.__player
+          break
+        trd += 1
+    except:
+      pass
+    trdcolumns.clear()
+    trdcolumns.clear()
+
+
+    
+    tld = 1
+    tldrows = []
+    tldcolumns = []
+    tlFound = False
+    try:
+      while tlFound == False and row-tld >= 0 and col-tld >= 0:
+        tldrows.append(row-tld)
+        tldcolumns.append(col-tld)
+        if self.__board[row-tld][col-tld] == self.__player:
+          tlFound = True
+          for i in range(len(tldrows)):
+            self.__board[tldrows[i]][tldcolumns[i]] = self.__player
+        tld += 1
+    except:
+      pass
+    tldrows.clear()
+    tldcolumns.clear()
+    
+    brd = 1
+    brdrows = []
+    brdcolumns = []
+    brFound = False
+    try:
+      while brFound == False and row+brd <= 7 and col+tld <= 7:
+        brdrows.append(row+brd)
+        brdcolumns.append(col+brd)
+        if self.__board[row+brd][col+brd] == self.__player:
+          brFound = True
+          for i in range(len(brdrows)):
+            self.__board[brdrows[i]][brdcolumns[i]] = self.__player
+        brd += 1
+    except:
+      pass
+    brdcolumns.clear()
+    brdrows.clear()
+
+    bld = 1
+    bldrows = []
+    bldcolumns = []
+    blFound = False
+    try:
+      while blFound == False and row+bld <= 7 and col-bld >= 0:
+        bldrows.append(row+bld)
+        bldcolumns.append(col-bld)
+        if self.__board[row+bld][col-bld] == self.__player:
+          blFound = True
+          for i in range(len(bldrows)):
+            self.__board[bldrows[i]][bldcolumns[i]] = self.__player
+        bld += 1
+    except:
+      pass
+    bldcolumns.clear()
+    bldrows.clear()
+
+    
     
 
-
-
-
-
+    
+    print("here")
     if self.__player == Game.p1: 
       self.__player = Game.p2
     else:
       self.__player = Game.p1
-    
+
+
     
   
 
